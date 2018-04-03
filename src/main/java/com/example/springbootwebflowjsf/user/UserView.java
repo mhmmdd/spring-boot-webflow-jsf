f144795e42f5a0dc33ac025b6412cb8a52819108
mhmmdd <mhmmd.dd@gmail.com>
2018-04-03 09:48:50 +0300

    Added datatable and CRUD operations.

--- src/main/java/com/example/springbootwebflowjsf/user/UserView.java
+++ src/main/java/com/example/springbootwebflowjsf/user/UserView.java
@@ -1,29 +1,80 @@
 package com.example.springbootwebflowjsf.user;
 
-import javax.annotation.ManagedBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ViewScoped;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
-@ManagedBean
+
+
+@Component
 @ViewScoped
-public class UserView {
+public class UserView implements Serializable {
 
-    private String firstname;
-    private String lastname;
 
-    public String getFirstname() {
-        return firstname;
+    @Autowired
+    private UserService userService;
+
+    private List<User> users = new ArrayList<>();
+
+    private User user;
+
+    @PostConstruct
+    public void init() {
+        user = new User();
+        if(userService !=null) {
+            users = userService.getAllUsers();
+
+        }
     }
 
-    public void setFirstname(String firstname) {
-        this.firstname = firstname;
+    public String prepareForUpdate(Long id){
+        user = userService.getUser(id).get();
+        return "new";
     }
 
-    public String getLastname() {
-        return lastname;
+
+    public String savePerson () {
+        userService.addUser(user);
+        users = new ArrayList<>();
+        users = userService.getAllUsers();
+        System.out.println("save");
+        return "save";
     }
 
-    public void setLastname(String lastname) {
-        this.lastname = lastname;
+    public String newPerson () {
+        user = new User();
+        System.out.println("new");
+        return "new";
+    }
+
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+
+    public UserService getUserService() {
+        return userService;
     }
 
 //    public void save() {
